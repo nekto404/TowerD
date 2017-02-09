26ff56cf05ea8cb756addf68142ef50d4ce43b47
nekto404 <gsmartrsh@gmail.com>
2017-02-09 15:54:10 +0200

    Create enemy and spawner
    Magic whit time update

---
+++ TD Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    
+    public float speed=100f;
+
+    private Transform waypoints;
+    private Transform waypoint;
+    private int waypointIndex=-1;
+	// Use this for initialization
+	void Start () {
+        waypoints = GameObject.Find("WayPoints").transform;
+        NextWayPoint();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = waypoint.transform.position - transform.position;
+        dir.y = 0;
+        float _speed = Time.deltaTime * speed;
+        transform.Translate(dir.normalized * Time.deltaTime * _speed);
+        if (dir.magnitude <= _speed/10)
+            NextWayPoint();
+
+    }
+
+    void NextWayPoint()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.childCount)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypoint = waypoints.GetChild(waypointIndex);
+    }
+}
---
+++ TD Project/Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour {
+
+    public GameObject spawnObject;
+    public float spawnTime=3f;
+    
+    private float timer=1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Instantiate(spawnObject, transform.position, transform.rotation);
+            timer = spawnTime;
+        }
+	}
+}
