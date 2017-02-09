920f408d6295d4a6ed56e11d3adc6e723d74e3df
nekto404 <gsmartrsh@gmail.com>
2017-02-10 01:25:26 +0200

    Add tower logic
    With magic too)

---
+++ TD Project/Assets/TowerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+    public Transform target;
+    public float range =2f;
+
+    public Transform partToRotate;
+    public float turnSpeed=50f;
+
+    public string enemyTag = "Enemy";
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget",0f,0.25f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position,enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if ( nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
