4e93801367b27cc28d9b93f80f147340c9d69e51
nekto404 <gsmartrsh@gmail.com>
2017-02-11 16:45:25 +0200

    Test commit

---
+++ TD Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Camera camera;
+    PlaceController place;
+    // Use this for initialization
+    void Start() {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        bool isMouseDown = Input.GetMouseButtonDown(0);
+        bool isMouseUp = Input.GetMouseButtonUp(0);
+
+        if (isMouseDown || isMouseUp)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag=="TowerPlace")
+                {
+                    PlaceController tp = hit.collider.gameObject.GetComponent<PlaceController>();
+                    if (isMouseDown)
+                        tp.MouseDown();
+                    else if (isMouseUp)
+                    {
+                        tp.MouseUp();
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ TD Project/Assets/Scripts/PlaceController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceController : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MouseDown()
+    {
+        GetComponent<Renderer>().material.color = new Color(1,0,0);
+        Debug.Log("Down");
+    }
+
+    public void MouseUp()
+    {
+        GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+        Debug.Log("Up");
+    }
+}
